a59cefaaa6de9450999be1734b3cdee19b6411d7
arani <aranimontes@protonmail.com>
2023-03-18 20:37:16 +0100

    new price feed based on tick & liquidity

--- PriceFeed.js
+++ PriceFeed.js
@@ -1,83 +1,91 @@
-const { request, gql } = require("graphql-request");
 const Web3 = require("web3");
+const web3 = new Web3("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"); // Replace YOUR_INFURA_PROJECT_ID with your Infura project ID
 
-// Replace with your Ethereum node URL
-const WEB3_PROVIDER_URL = "https://mainnet.infura.io/v3/YOUR-PROJECT-ID";
+const poolAddresses = [
+  "0x...", // Pool address 1
+  "0x...", // Pool address 2
+  "0x...", // Pool address 3
+  // Add more pool addresses as needed
+];
 
-// Object with pool addresses as keys and arrays of target prices as values
-const POOL_TARGET_PRICES = {
-  "0x123...": [100, 200],
-  "0x456...": [300, 400],
-  "0x789...": [500, 600],
-};
+const targetPrices = [
+  {
+    user: "User 1",
+    pool: "0x...", // Pool address
+    targetPrice: "1000000000000000000", // Target price in wei
+    isHigher: true, // True if target price is higher than current price, false otherwise
+    isSurpassed: false, // True if target price has been surpassed, false otherwise
+  },
+  {
+    user: "User 2",
+    pool: "0x...", // Pool address
+    targetPrice: "2000000000000000000", // Target price in wei
+    isHigher: false, // True if target price is higher than current price, false otherwise
+    isSurpassed: false, // True if target price has been surpassed, false otherwise
+  },
+  // Add more target prices as needed
+];
 
-// GraphQL query to fetch pool data
-const query = gql`
-  query PoolQuery($poolAddress: Bytes!) {
-    pool(id: $poolAddress) {
-      token0 {
-        symbol
+async function trackPrices() {
+  const blockNumber = await web3.eth.getBlockNumber();
+  console.log(`Current block number: ${blockNumber}`);
+
+  for (const poolAddress of poolAddresses) {
+    const poolContract = new web3.eth.Contract(
+      UNISWAP_V3_POOL_ABI,
+      poolAddress
+    );
+    const [tick, liquidity] = await Promise.all([
+      poolContract.methods
+        .slot0()
+        .call()
+        .then((slot0) => slot0.tick),
+      poolContract.methods.liquidity().call(),
+    ]);
+
+    const sqrtPriceX96 = Math.sqrt(Math.pow(1.0001, tick) * 2 ** 96);
+    const price = (liquidity * 2 * sqrtPriceX96) / 2 ** 128;
+
+    console.log(`Pool ${poolAddress}: ${price}`);
+
+    for (const targetPrice of targetPrices) {
+      if (targetPrice.pool === poolAddress) {
+        const currentPriceBN = web3.utils.toBN(price);
+        const targetPriceBN = web3.utils.toBN(targetPrice.targetPrice);
+
+        if (
+          targetPrice.isHigher &&
+          !targetPrice.isSurpassed &&
+          currentPriceBN.gte(targetPriceBN)
+        ) {
+          console.log(
+            `Target price of ${targetPrice.targetPrice} surpassed for ${targetPrice.user}`
+          );
+          // Execute the function for the user whose target price has been surpassed
+          // TODO: Implement the function execution here
+          targetPrice.isSurpassed = true;
+        } else if (
+          !targetPrice.isHigher &&
+          targetPrice.isSurpassed &&
+          currentPriceBN.lt(targetPriceBN)
+        ) {
+          console.log(
+            `Target price of ${targetPrice.targetPrice} not surpassed anymore for ${targetPrice.user}`
+          );
+          // TODO: Implement the function execution here
+          targetPrice.isSurpassed = false;
+        }
       }
-      token1 {
-        symbol
-      }
-      tick
-      sqrtPrice
     }
   }
-`;
+}
 
-// GraphQL endpoint for Uniswap V3 subgraph
-const endpoint = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
-
-// Create a Web3 instance to listen for new blocks
-const web3 = new Web3(WEB3_PROVIDER_URL);
-
-// Function to fetch and process pool data for a given pool address
-async function trackPoolPrice(poolAddress) {
-  try {
-    const data = await request(endpoint, query, { poolAddress });
-    const { token0, token1, tick, sqrtPrice } = data.pool;
-    const price =
-      (Math.pow(sqrtPrice, 2) * Math.pow(10, tick)) / Math.pow(2, 96);
-    console.log(`Price of ${token0.symbol}/${token1.symbol}: ${price}`);
-
-    // Check if the pool price has exceeded any of the target prices
-    const targetPrices = POOL_TARGET_PRICES[poolAddress];
-    if (
-      targetPrices &&
-      targetPrices.some((targetPrice) => price >= targetPrice)
-    ) {
-      console.log(
-        `Pool ${poolAddress} has exceeded one or more target prices!`
-      );
-      await callMyFunction();
+web3.eth
+  .subscribe("newBlockHeaders", (error, result) => {
+    if (!error) {
+      trackPrices();
+    } else {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(`Error fetching pool data for ${poolAddress}:`, error);
-  }
-}
-
-// Function to loop through pool addresses and track their prices
-async function trackPoolPrices() {
-  for (const poolAddress in POOL_TARGET_PRICES) {
-    await trackPoolPrice(poolAddress);
-  }
-}
-
-// Function to call your custom function
-async function callMyFunction() {
-  // Replace with your custom function code
-  console.log("Calling my function...");
-}
-
-// Listen for new blocks and update pool prices on each block
-web3.eth.subscribe("newBlockHeaders", async (error, result) => {
-  if (error) {
-    console.error("Error listening for new blocks:", error);
-    return;
-  }
-
-  console.log(`New block detected: ${result.number}`);
-  await trackPoolPrices();
-});
+  })
+  .on("error", console.error);
